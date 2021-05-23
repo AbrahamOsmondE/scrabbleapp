eb7296dc328d7c8f55c4422990f2703e6480b856
AbrahamOsmondE <abraham.osmond@gmail.com>
2021-05-23 18:32:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import HomePage from "./container/HomePage";
 import AnalysisPage from "./container/AnalysisPage";
 
  const rootElement = document.getElementById("root");
  ReactDOM.render(
-   <HashRouter>
+   <BrowserRouter>
     <Switch>
      <Route exact path="/" component={HomePage} />
      <Route path="/AnalysisPage" component={AnalysisPage} />
    </Switch>
-   </HashRouter>,
+   </BrowserRouter>,
    rootElement
  );
